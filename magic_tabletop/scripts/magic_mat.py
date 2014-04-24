12f6e94006b27b6d7a39c98adbf69fb2e7a36c91
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-04-24 12:28:25 -0700

    Magic mat demo filters for pink paper.

--- magic_tabletop/scripts/magic_mat.py
+++ magic_tabletop/scripts/magic_mat.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python
-#
-# Makes stuff higher than the floor disappear. Steps:
-# 1) Grabs a Kinect PointCloud2 msg
-# 2) Finds the dominant plane -- the floor, if the Kinect is pointed down
-# 3) Projects all points above that (floor) plane into the floor
-# 4) Paints those points a different color
-# 5) Converts back to an Image msg, which is displayed to the user as video
 
 import rospy
 from sensor_msgs.msg import PointCloud2, Image
@@ -15,8 +8,7 @@ import numpy
 import matplotlib
 import copy
 
-from magic_tabletop.srv import *
-
+import code # FOR TESTING
 
 class MatFilter():
     def __init__(self, topic):
@@ -28,25 +20,57 @@ class MatFilter():
         """ Get image to which we're subscribed. """
         image_cv = self.bridge.imgmsg_to_cv(image_in, 'bgr8')
         image_cv2 = numpy.array(image_cv, dtype=numpy.uint8)
-        image_float = (image_cv2.astype(float) + 1) / 256
-        image_hsv = ((matplotlib.colors.rgb_to_hsv(image_float) * 256) - 1).astype('uint8')
+        #image_float = (image_cv2.astype(float) + 1) / 256
+        #image_hsv = ((matplotlib.colors.rgb_to_hsv(image_float) * 256) - 1).astype('uint8')
+        image_hsv = cv2.cvtColor(image_cv2, cv2.COLOR_BGR2HSV)
+        image_hsv = cv2.blur(image_hsv, (5, 5))
+
+        #is_pink = cv2.inRange(image_hsv, numpy.array((130, 50, 120)), numpy.array((175, 85, 200)))
+        is_pink = cv2.inRange(image_hsv, numpy.array((130, 50, 120)), numpy.array((175, 255, 255)))
         
-        #print image_cv2.shape
-        #print image_hsv[250, 250, :]  # print something in the middle
+        #code.interact(local=locals())
 
+        for dim in range(3): image_hsv[:,:,dim] *= is_pink / 255
+        #print is_pink
 
-        is_pink = (image_hsv[:, :, 0] > 200) & (image_hsv[:, :, 0] < 240) & (image_hsv[:, :, 1] > 30)
-        pink = copy.deepcopy(image_hsv)
-        for dim in range(3): pink[:, :, dim] *= is_pink
-        pink_avg = numpy.mean(numpy.mean(pink, 0), 0)  # mean HSV vector for pink bits of the image
-        print pink_avg
+        #is_pink = (image_hsv[:, :, 0] > 200) & (image_hsv[:, :, 0] < 240) & (image_hsv[:, :, 1] > 35) & (image_hsv[:, :, 2] > 100)
+        #pink = copy.deepcopy(image_float)
+        #for dim in range(3): pink[:, :, dim] = is_pink*1.0
+        #pink = cv2.medianBlur(pink, 5)
+        """
+        contours = cv2.findContours(is_pink, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        print contours
+        
+        # finding contour with maximum area and store it as best_cnt
+        max_area = 10000
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > max_area:
+                max_area = area
+                best_contour = contour
+
+        best_index = contours.index(best_contour)
+        rospy.loginfo('Best contour was contour #{0} with area of {1}'.format(best_index, max_area))
+        cv2.drawContours(image_cv2, contours, best_index, numpy.array([0,0,0]))  # draw black line around largest pink blob
+        """
+        
+        #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        #pink = cv2.morphologyEx(pink, cv2.MORPH_CLOSE, kernel, borderValue=255, iterations=1)
+
+        
+
+        #pink = copy.deepcopy(image_hsv)
+        #for dim in range(3): pink[:, :, dim] *= is_pink
+        #pink_avg = numpy.mean(numpy.mean(pink, 0), 0)  # mean HSV vector for pink bits of the image
+        #print pink_avg
 
         #for dim in range(3): image_cv2[:, :, dim] = numpy.uint8(green)*255
         
         # Convert back to ROS Image msg
-        image_hsv_float = (pink.astype(float) + 1) / 256
-        image_rgb = ((matplotlib.colors.hsv_to_rgb(image_hsv_float) * 256) - 1).astype('uint8')
-        image_cv = cv.fromarray(image_rgb)
+        #image_hsv_float = (pink.astype(float) + 1) / 256
+        #image_rgb = ((matplotlib.colors.hsv_to_rgb(image_hsv_float) * 256) - 1).astype('uint8')
+        image_cv2 = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
+        image_cv = cv.fromarray(image_cv2)
         image_out = self.bridge.cv_to_imgmsg(image_cv, 'bgr8')
         self.pub.publish(image_out)
 
