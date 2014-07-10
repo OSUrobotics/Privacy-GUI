3576f3936a7465e72ec399f80da64002684241b1
Alex Hubers <ahubers15@cornellcollege.edu>
2014-07-10 13:24:27 -0700

    fixed timeTester.py bugs.

--- remote_nav/scripts/timeTester.py
+++ remote_nav/scripts/timeTester.py
@@ -4,11 +4,11 @@ import rospy
 import datetime
 
 def tf_callback(data):
-	print ( "Rospy time: " + rospy.Time.now())
-	print ( "datetime: " + datetime.datetime.now() ) 
+	print ( "Rospy time: " + str(rospy.get_time()) )
+	print ( "datetime: " + str( datetime.datetime.now()) ) 
 
 if __name__ == '__main__':
-	rospy.init_node('timeTester.py')
+	rospy.init_node('timeTester')
 	tf_sub = rospy.Subscriber("/tf", tf.msg.tfMessage, tf_callback)
 	rospy.spin()
 
