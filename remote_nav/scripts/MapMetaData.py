68dc563d694183107001a5b94a3bbd512282cee2
petScarecrow <pcbiggs@wpi.edu>
2014-07-25 10:22:25 -0700

    Adds RegistredMapMetaData type so that sort of yaml can be read

--- remote_nav/scripts/MapMetaData.py
+++ remote_nav/scripts/MapMetaData.py
@@ -12,6 +12,21 @@ class MapMetaData(yaml.YAMLObject):
 		self.occupied_thresh = occupied_thresh
 		self.free_thresh = free_thresh
 
+class RegisteredMapMetaData(yaml.YAMLObject):
+	yaml_tag = u'!RegisteredMapMetaData'
+
+	def __init__(self, semantic_map, slam_map, origin, resolution, slam_width, slam_height, semantic_width, semantic_height, semantic_to_slam, slam_to_semantic, objects):
+		self.slam_map = slam_map
+		self.semantic_map = semantic_map
+		self.resolution = resolution
+		self.origin = origin
+		self.slam_width = slam_width
+		self.slam_height = slam_height
+		self.semantic_height = semantic_height
+		self.semantic_width = semantic_width
+		self.slam_to_semantic = slam_to_semantic
+		self.semantic_to_slam = semantic_to_slam
+
 def yaml_to_meta_data(file_name):
 	# Open the file -- no error cehcking here
 	fo = open(file_name)
