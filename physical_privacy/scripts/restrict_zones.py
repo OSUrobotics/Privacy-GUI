385416ee969c400e8534f90cd79b9989165f38d4
Jasper LaFortune <lafortuj@onid.orst.edu>
2014-08-13 16:56:42 -0700

    fixed srv generation bugs

--- physical_privacy/scripts/restrict_zones.py
+++ physical_privacy/scripts/restrict_zones.py
@@ -7,5 +7,11 @@ import rospy
 # External Libraries
 
 
+class ZoneRestrictor():
+	"""Class that creates a privacy restricted costmap from privacy zones"""
+	def __init__(self, arg):
+		self.arg = arg
+		
+
 if __name__ == "__main__":
 	pass
