b50f8a52b06b5e4570b457c5905e7eaae67ea32f
Alex Hubers <ahubers15@cornellcollege.edu>
2014-08-14 10:01:32 -0700

    Fixed amcl.launch and restructured restrict_zones_client

--- physical_privacy/scripts/restrict_zones_client.py
+++ physical_privacy/scripts/restrict_zones_client.py
@@ -5,9 +5,16 @@ import pprint
 from geometry_msgs.msg import Polygon, Point32
 from physical_privacy.srv import restrictZones
 
-if __name__ == '__main__':
+def restrict_zones_client(polygon_list):
 	rospy.wait_for_service('restrict_zones')
+	
 	restrict_zones = rospy.ServiceProxy('restrict_zones', restrictZones)
+	
+	response = restrict_zones(polygon_list)
+	return response
+		
+
+if __name__ == '__main__':
 	# We use rospack to find the filepath for physical_privacy, lest the user does not roslaunch.
 	rospack = rospkg.RosPack()
 	package_path = rospack.get_path('physical_privacy')
@@ -16,17 +23,16 @@ if __name__ == '__main__':
 	zones_file = rospy.get_param('priv_nav/zones', package_path + "/zones/test.yaml")
 	with open(zones_file) as f:
 		zones = yaml.safe_load(f)
-
-	polygon_list = []
-	for zone in zones['Zone List']:
-		polygon = Polygon()
-		for point in zone['Points']:
-			new_point = Point32()
-			new_point.x = point['x'] 
-			new_point.y = point['y']
-			polygon.points.append(new_point)
-		polygon_list.append(polygon)
-	response = restrict_zones(polygon_list)
+		polygon_list = []
+		for zone in zones['Zone List']:
+			polygon = Polygon()
+			for point in zone['Points']:
+				new_point = Point32()
+				new_point.x = point['x'] 
+				new_point.y = point['y']
+				polygon.points.append(new_point)
+			polygon_list.append(polygon)
+		restrict_zones_client(polygon_list)
 
 
 
