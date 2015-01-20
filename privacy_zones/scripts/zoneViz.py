8590595586096b8d13d452f17b86ff360680b4f4
Dan Lazewatsky <dan@lazewatsky.com>
2015-01-20 17:07:55 -0600

    change zone colors based on transitions

--- privacy_zones/scripts/zoneViz.py
+++ privacy_zones/scripts/zoneViz.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 
 from privacy_zones.map_geometry import MapGeometry, Zones
+from privacy_zones.msg import Transition
 import sys
 import yaml
 from easy_markers.generator import MarkerGenerator
 from visualization_msgs.msg import Marker, MarkerArray
 import rospy
 
-if __name__ == '__main__':
+active_zones = {}
 
+def transition_cb(msg):
+    if msg.action == Transition.ENTER:
+        active_zones[msg.zone.name] = [0,0,1,1]
+    elif msg.action == Transition.EXIT and msg.zone.name in active_zones:
+        del active_zones[msg.zone.name]
+
+if __name__ == '__main__':
     rospy.init_node('zone_viz')
     marker_pub = rospy.Publisher('/visualization_marker_array', MarkerArray)
+    rospy.Subscriber('transition', Transition, transition_cb)
+
 
     zone_file_path, map_info_path = sys.argv[1], sys.argv[2]
     with open(zone_file_path, 'r') as zone_file:
@@ -19,8 +29,8 @@ if __name__ == '__main__':
     geom = MapGeometry(map_info_path)
 
     z = Zones(zones, geom)
-    marker_array = z.to_marker_array()
 
     while not rospy.is_shutdown():
+        marker_array = z.to_marker_array(colors=active_zones)
         marker_pub.publish(marker_array)
         rospy.sleep(0.25)
