a85b6eae5a3d97ee92586d72bec3eaf6f34db294
Dan Lazewatsky <dan@lazewatsky.com>
2015-01-20 17:06:32 -0600

    subscribe to odom; transform pose to map frame

--- privacy_zones/scripts/zoneServer.py
+++ privacy_zones/scripts/zoneServer.py
@@ -3,10 +3,12 @@
 from privacy_zones.map_geometry import MapGeometry, Zones
 from privacy_zones.msg import Transition
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
 from shapely.geometry import Point
 import sys
 import yaml
 import rospy
+import tf
 
 class ZoneServer(object):
     def __init__(self, zone_file_path, map_info_path):
@@ -20,16 +22,25 @@ class ZoneServer(object):
         self.last_transition_time = rospy.Time(0)
 
         rospy.Subscriber('pose', PoseStamped, self.pose_cb)
+        rospy.Subscriber('odom', Odometry, self.odom_cb)
         self.transition_pub = rospy.Publisher('transition', Transition)
 
+        self.tfl = tf.TransformListener()
+
     def pose_cb(self, msg):
-        # TODO transform this to the map frame
-        pt = Point(msg.pose.position.x, msg.pose.position.y)
+        msg_trans = self.tfl.transformPose('map', msg)
+        pt = Point(msg_trans.pose.position.x, msg_trans.pose.position.y)
         within_zones = self.zones.in_which(pt)
         if within_zones:
             self.previous_zones = self.current_zones
             self.current_zones = within_zones
             self.check_transition()
+
+    def odom_cb(self, msg):
+        pose = PoseStamped()
+        pose.header = msg.header
+        pose.pose = msg.pose.pose
+        self.pose_cb(pose)
 
     def check_transition(self):
         if self.previous_zones and self.current_zones:
