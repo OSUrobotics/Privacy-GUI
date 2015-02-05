f1d53bc1f65dcf1452d194be9e66405caf33359d
Dan Lazewatsky <dan@lazewatsky.com>
2015-02-05 13:04:28 -0600

    update service name

--- privacy_zones/scripts/make_zone_peac_refs.py
+++ privacy_zones/scripts/make_zone_peac_refs.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env rosh
 import yaml
 zone_controls = {}
-wait_for_service('list_locations')
-for loc in services.list_locations().locations:
-    for dev in services.list_devices(loc.locationId).devices:
+wait_for_service('peac/list_locations')
+for loc in services.peac.list_locations().locations:
+    for dev in services.peac.list_devices(loc.locationId).devices:
         zone_controls['ZONE%s' % dev.deviceId] = []
-        for cont in services.get_device_info(dev.deviceId).controls:
+        for cont in services.peac.get_device_info(dev.deviceId).controls:
             zone_controls['ZONE%s' % dev.deviceId].append({
             'location'  : loc.name,
             'device'    : dev.name,
